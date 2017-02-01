aaea74303deef78c0e698cd6b2a6a4ce21885a60
Eric Erhardt <eric.erhardt@microsoft.com>
2017-01-31 18:08:00 -0600

    Initial System.Unix project structure.

---
+++ src/System.Unix/src/System/Unix/Native/Syscall.Unix.cs
@@ -0,0 +1,14 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Unix.Native
+{
+    public static partial class Syscall
+    {
+        public static int chmod(string path, FilePermissions mode)
+        {
+            return Interop.Sys.ChMod(path, (int)mode);
+        }
+    }
+}
